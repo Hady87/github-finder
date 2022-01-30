61a0097d2cb8ea0f633c4aa9af6d7e936925e3ec
Hady <hady.etd@gmail.com>
2022-01-30 18:01:06 -0500

    Section 11

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -1,102 +1,23 @@
-//import { useEffect, useState } from "react";
 import { createContext, useReducer } from 'react'
 import githubReducer from './GithubReducer'
 
 const GithubContext = createContext()
-const GITHUB_URL= process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN= process.env.REACT_APP_GITHUB_TOKEN
-
 
 export const GithubProvider = ({ children }) => {
-const initialState={
-users:[],
-loiding:false,
-repos:[],
-user:{},
-}
-const [state,dispatch] = useReducer(githubReducer,initialState)
-
-// Get search users
-const searchUsers = async (text) => {
-  Setloiding()
-  const params =new URLSearchParams({
-    q:text
-  })
-  
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-    const {items} = await response.json();
-    dispatch({
-      type:"GET_USERS",
-      payload:items,
-    })
-  };
-  // Get a single user
-const getUser = async (login) => {
-  Setloiding()
-  
-  
-    const response = await fetch(`${GITHUB_URL}/user${login}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-    if(response.status===404){
-      window.Location='/notfound'
-    } 
-    else{
-const data = await response.json();
-    dispatch({
-      type:"GET_USER",
-      payload:data,
-    })
-    }
-    
-  };
-  // Get user repos
-  const getUserRepos = async (login) => {
-  Setloiding()
-  const params =new URLSearchParams({
-    sort:'created',
-    per_page:10,
-  })
-  
-  
-    const response = await fetch(`${GITHUB_URL}/users?${login}/repos?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-    const data = await response.json();
-    dispatch({
-      type:"GET_REPOS",
-      payload:data,
-    })
-  };
-  //clear users
-  const clearUsers=()=>{
- 
-   dispatch({type:'CLEAR_USERS'})
- 
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
   }
- // Set loiding
- const Setloiding=()=>{
-   dispatch({type:'SET_LOADING'})
- }
+
+  const [state, dispatch] = useReducer(githubReducer, initialState)
+
   return (
     <GithubContext.Provider
       value={{
-        users:state.users,
-        loiding:state.loiding,
-        user:state.user,
-        repos:state.repos,
-        searchUsers,
-        clearUsers,
-        getUser,
-        getUserRepos,
+        ...state,
+        dispatch,
       }}
     >
       {children}
@@ -105,4 +26,3 @@ const data = await response.json();
 }
 
 export default GithubContext
-
